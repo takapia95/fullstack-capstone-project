397aa0d30bd2a96a15d2787ad8efd83c86175356
takapia95 <113641497+takapia95@users.noreply.github.com>
2024-09-02 20:57:00 -0400

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -8,7 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
-  const navigate = useNavigate();
   return (
         <>
         <Navbar/>
