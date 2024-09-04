ab2546b10f4806a84cde4777baa2dc3a6e9d9854
takapia95 <113641497+takapia95@users.noreply.github.com>
2024-09-04 18:47:55 -0400

    Add files via upload
    update backend

---
+++ giftlink-backend/models/db.js
@@ -0,0 +1,23 @@
+// db.js
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+
+// MongoDB connection URL with authentication options
+let url = `${process.env.MONGO_URL}`;
+
+const dbName = "giftdb";
+
+async function connectToDatabase() {
+  const client = new MongoClient(url);
+
+  // Task 1: Connect to MongoDB
+  await client.connect();
+
+  // Task 2: Connect to database giftDB and store in variable dbInstance
+  const dbInstance = client.db(dbName);
+
+  // Task 3: Return database instance
+  return dbInstance;
+}
+
+module.exports = connectToDatabase;
